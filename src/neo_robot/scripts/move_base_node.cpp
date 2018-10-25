fe48e8ae3a16151263792c920c89bfd0181047ee
Your Name Here <padhupradheep@gmail.com>
2018-10-25 16:33:10 +0530

    Visualisation marker array has been added

--- src/neo_robot/scripts/move_base_node.cpp
+++ src/neo_robot/scripts/move_base_node.cpp
@@ -17,6 +17,8 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
 using namespace std;
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -26,6 +28,8 @@ int id = 0;
 MOVE_base::MOVE_base(){}
 
 MOVE_base::~MOVE_base(){}
+
+/* Determing te status part is incomplete */
 
 void MOVE_base::status_cb(const actionlib_msgs::GoalStatusArray::ConstPtr &msg)
 {
@@ -40,6 +44,8 @@ void MOVE_base::simple_client(const geometry_msgs::Pose msg)
 
 /* 
 Move base client is being called up in this function
+
+Here we send the goals
 */	
 
 	MoveBaseClient ac("move_base", true);
@@ -64,11 +70,11 @@ Move base client is being called up in this function
 
   	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
    	
-   	ROS_INFO("Hooray, the base moved 1 meter forward");
+   	ROS_INFO("Hooray, the base moved to the desired goal");
 
   else
 
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to move to the desired goal");
 
 }
 
@@ -76,26 +82,63 @@ Move base client is being called up in this function
 
 int main(int argc, char **argv)
 {
-	MOVE_base MB;
+	MOVE_base MB; //Creating an object
 	ros::init(argc, argv, "move_base_node");
 	ros::NodeHandle n;
-	ros::MultiThreadedSpinner spinner(2);
-	// n.getParam("move_base_node/goal1",MB.goals);
+	ros::Rate loop_rate_markers(20);
+
+    int step_counter = 0;
+    visualization_msgs::Marker marker;
+    visualization_msgs::MarkerArray markers;
+    marker.id = 0;
+
+    ros::Publisher marker_pub = \
+	n.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 100); // Creating an array
+
+//Using XmlRpcValue to parse the XML file
 	XmlRpc::XmlRpcValue my_list;
 	n.getParam("move_base_node/my_list", my_list);
 	ROS_INFO_STREAM("size:"<<my_list.size());
 	ros::Rate r(10);
-	// MB.sub_status = n.subscribe<actionlib_msgs::GoalStatusArray>("/move_base/status", 1, &MOVE_base::status_cb, &MB);
-	int i = 0;
+
+	ROS_INFO_STREAM("The marker publisher has connected to subscribers.");
+
+	/* Visualizing the marker */
+	/*This will work well with a new node*/
 	for(int i = 0; i<my_list.size(); i++)
+	{	
+		marker.header.frame_id = "/map";
+	    marker.type = visualization_msgs::Marker::SPHERE;
+	    marker.action = visualization_msgs::Marker::ADD;
+	    marker.pose.position.x = my_list[i][0];
+	    marker.pose.position.y = my_list[i][0];
+	    marker.pose.position.z = 0.0;
+	    marker.pose.orientation.x = 0.0;
+	    marker.pose.orientation.y = 0.0;
+	    marker.pose.orientation.z = 0.0;
+	    marker.pose.orientation.w = 0.0;
+	    marker.scale.x = 0.2;
+	    marker.scale.y = 0.2;
+	    marker.scale.z = 0.2;
+	    marker.color.r = 0.0;
+	    marker.color.g = 2.0;
+	    marker.color.b = 0.0;
+	    marker.color.a = 1.0;
+	    marker.lifetime = ros::Duration();
+	    marker.id+=1;
+		markers.markers.push_back(marker);
+   	}
+
+	for(int j = 0; j<my_list.size(); j++)
 		{
-		ROS_INFO_STREAM("The goals that the Robot will travel are: " << my_list[i]);
-		MB.poses.position.x = my_list[i][0];
-		MB.poses.position.y = my_list[i][1];
-		MB.poses.orientation.z = my_list[i][2];
+		ROS_INFO_STREAM("The goals that the Robot will travel are: " << my_list[j]);
+		marker_pub.publish(markers); 
+		MB.poses.position.x = my_list[j][0];
+		MB.poses.position.y = my_list[j][1];
+		MB.poses.orientation.z = my_list[j][2];
 		MB.simple_client(MB.poses);
 		ros::spinOnce();
 		r.sleep();
 		}
-
+	 ros::shutdown();
 }
